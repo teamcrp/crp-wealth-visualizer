d8241683531928574141d4f19bce86cb1eedae46
team CRP <gpt4crp@gmail.com>
2025-04-26 21:28:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/', // 👈 ADD THIS LINE for GitHub Pages or custom domain!
+  base: './', // Change this to relative paths
   server: {
     host: "::",
     port: 8080,
